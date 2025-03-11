8b3ea63bd7cdb6db3682ed5bd9d45d80eba27fa3
Matthieu Ciappara <ciappa_m@modulotech.fr>
2025-03-11 13:02:44 +0100

    Update `turbo_scopes` method for compatibility with versioning
    Refactor the `turbo_scopes` method to handle differences between Trestle versions below and above `0.10.1`. Introduced conditional logic to ensure proper behavior and parameter handling based on the version in use. Adjusted method signatures and calls accordingly for consistency and functionality.

--- lib/trestle/turbo_scopes/builder.rb
+++ lib/trestle/turbo_scopes/builder.rb
@@ -1,15 +1,30 @@
 module Trestle
   module TurboScopes
     module Builder
-      def turbo_scopes(options={}, &block)
-        admin.define_adapter_method(:turbo_scopes, &block)
+      if Trestle::VERSION < '0.10.1'
+        def turbo_scopes(options = {}, &block)
+          admin.define_adapter_method(:turbo_scopes, &block)
 
-        admin.additional_routes << proc do
-          get :turbo_scopes, on: :collection
+          admin.additional_routes << proc do
+            get :turbo_scopes, on: :collection
+          end
+
+          admin.scopes.options.merge!(options)
+          admin.scopes.append(options, &block) if block_given?
         end
+      else
+        def turbo_scopes(defaults: {}, **options, &block)
+          defaults = defaults.merge(options.slice(:count))
 
-        admin.scopes.options.merge!(options)
-        admin.scopes.append(options, &block) if block_given?
+          admin.define_adapter_method(:turbo_scopes, &block)
+
+          admin.additional_routes << proc do
+            get :turbo_scopes, on: :collection
+          end
+
+          admin.scopes.apply_options!(options)
+          admin.scopes.append(**defaults, &block) if block_given?
+        end
       end
     end
   end
